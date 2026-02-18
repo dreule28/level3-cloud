93a1b703324698611c51d632664a08806e2cd70e
danielCluster <danielreule28@gmail.com>
2026-02-18 14:42:53 +0100

    feat: update API endpoints to remove redundant 'api' prefix

--- Week_4/paas-ui/src/api/auth.js
+++ Week_4/paas-ui/src/api/auth.js
@@ -1,7 +1,7 @@
 import { http, setToken, clearToken } from "./http";
 
 export async function login(username, password) {
-  const data = await http.post("/api/auth/login", { username, password });
+  const data = await http.post("/auth/login", { username, password });
   // data = { access_token, token_type, expires_in }
   setToken(data.access_token);
   return data;
--- Week_4/paas-ui/src/api/instances.js
+++ Week_4/paas-ui/src/api/instances.js
@@ -1,17 +1,17 @@
 import { http } from "./http";
 
 export function listInstances() {
-  return http.get("/api/instances");
+  return http.get("/instances");
 }
 
 export function getInstance(id) {
-  return http.get(`/api/instances/${encodeURIComponent(id)}`);
+  return http.get(`/instances/${encodeURIComponent(id)}`);
 }
 
 export function createInstance(id) {
-  return http.post("/api/instances", { id });
+  return http.post("/instances", { id });
 }
 
 export function deleteInstance(id) {
-  return http.del(`/api/instances/${encodeURIComponent(id)}`);
+  return http.del(`/instances/${encodeURIComponent(id)}`);
 }
